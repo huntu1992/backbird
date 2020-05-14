1335c325c4237895b4f690b6f9453b0aaf236ec9
jym <cym19921224@gmail.com>
2020-05-14 13:34:13 +0900

    배포

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ const session = require('express-session');
 const cookie = require('cookie-parser');
 const morgan = require('morgan');
 
+const hpp = require('hpp');
+const helmet = require('helmet');
+
+const dotenv = require('dotenv');
+
+const prod = process.env.NODE_ENV === 'production';
 const db = require('./models');
 const passportConfig = require('./passport');
 const userRouter = require('./routes/user');
@@ -13,28 +19,43 @@ const postsRouter = require('./routes/posts');
 const hashtagRouter = require('./routes/hashtag');
 const app = express();
 
+dotenv.config();
 db.sequelize.sync();
 passportConfig();
 
-app.use(morgan('dev'));
-app.use(
-  cors({
-    origin: 'http://localhost:3080',
-    credentials: true,
-  })
-);
+if (prod) {
+  app.use(helmet());
+  app.use(hpp());
+  app.use(morgan('combined'));
+  app.use(
+    cors({
+      origin: 'http://bunga23.com',
+      credentials: true,
+    })
+  );
+} else {
+  app.use(morgan('dev'));
+  app.use(
+    cors({
+      origin: 'http://localhost:3080',
+      credentials: true,
+    })
+  );
+}
+
 app.use('/', express.static('uploads'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookie('cookiesecret'));
+app.use(cookie(process.env.COOKIE_SECRET));
 app.use(
   session({
     resave: false,
     saveUninitialized: false,
-    secret: 'cookiesecret',
+    secret: process.env.COOKIE_SECRET,
     cookie: {
       httpOnly: true,
       secure: false,
+      domain: prod && '.bunga23.com',
     },
   })
 );
@@ -55,6 +76,8 @@ app.post('/post', (req, res) => {
   }
 });
 
-app.listen(3085, () => {
-  console.log(`백엔드 서버 ${3085}번 포트에서 작동중.`);
+app.listen(prod ? process.env.PORT : 3085, () => {
+  console.log(
+    `백엔드 서버 ${prod ? process.env.PORT : 3085}번 포트에서 작동중.`
+  );
 });
